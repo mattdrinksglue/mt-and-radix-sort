a5d5511e0f3c0281a1f1581290b662769a0e8e82
matt <myounger009@gmail.com>
2021-12-01 08:48:36 +0000

    main

--- radix_sort.rs
+++ radix_sort.rs
@@ -99,9 +99,7 @@ fn radix_sort(arr: Vec<u32>, base: u32) -> Vec<u32> {
     let mut output = arr;
 
     let mut digit = 0;
-    let mut exp = 1;
-    while max / exp > 0 {
-        exp = base.pow(digit);
+    while max as u64 > (base as u64).pow(digit) {
         let len = output.len();
         output = counting_sort(&mut output, len, base, digit);
         digit += 1;
@@ -110,21 +108,114 @@ fn radix_sort(arr: Vec<u32>, base: u32) -> Vec<u32> {
     output
 }
 
-fn  main() {
-    // generate a list of pseudo-random numbers
-    let mut rand: MT = MT::new(1234);
+fn counting_sort_16(arr: &mut [u32], arr_len: usize, nth_digit: u32) -> Vec<u32> {
+    let mut output = vec![0; arr_len];
 
-    let range = 10;
-    let mut arr = vec![0; range];
-    for i in 0..range {
-        arr[i] = rand.next_rand() % 100000;
+    let mut digit_count = vec![0; 16];
+
+    let exp: u32 = 16_u32.pow(nth_digit);
+
+    for i in 0..arr_len {
+        let index = arr[i] / exp;
+        digit_count[(index & 0xF) as usize] += 1;
     }
 
+    for i in 1..16 as usize {
+        digit_count[i] += digit_count[i-1];
+    }
 
-    print_vec(&arr);
+    for i in (0..arr_len).rev() {
+        let index = arr[i] / exp;
+        output[digit_count[(index & 0xF) as usize] - 1] = arr[i];
+        digit_count[(index & 0xF) as usize] -= 1;
+    }
+
+    output
+}
+
+fn radix_sort_16(arr: Vec<u32>) -> Vec<u32> {
+    let max: u32 = match arr.iter().max() {
+        Some(x) => *x,
+        _ => panic!("Empty array provided")
+    };
+
+    let mut output = arr;
+
+    let mut digit = 0;
+    while digit < 8 && u64::from(max) > 16_u64.pow(digit) {
+        let len = output.len();
+        println!("Beginning csort: {}", 16_u32.pow(digit));
+        output = counting_sort_16(&mut output, len, digit);
+        digit += 1;
+    }
+
+    output
+}
+
+fn counting_sort_bitmask(arr: &mut [u32], arr_len: usize, exp: u32) -> Vec<u32> {
+    let mut output = vec![0; arr_len];
+
+    let mut digit_count = vec![0; exp as usize];
+
+    for i in 0..arr_len {
+        let index = arr[i] / exp;
+        digit_count[(index & (exp-1)) as usize] += 1;
+    }
+
+    for i in 1..exp as usize {
+        digit_count[i] += digit_count[i-1];
+    }
+
+    for i in (0..arr_len).rev() {
+        let index = arr[i] / exp;
+        output[digit_count[(index & (exp-1)) as usize] - 1] = arr[i];
+        digit_count[(index & (exp-1)) as usize] -= 1;
+    }
+
+    output
+}
+
+// radix sort using a bitmask to speed it up a little
+// takes in an exponent of 2, such that the mask can be (2^exp)-1
+// and bitwise & can be performed
+fn radix_sort_bitmask(arr: Vec<u32>, exp: u32) -> Vec<u32> {
+    let max: u32 = match arr.iter().max() {
+        Some(x) => *x,
+        _ => panic!("Empty array provided")
+    };
+
+    let mut output = arr;
+
+    let base_exp: u32 = 2_u32.pow(exp);
+
+    let mut digit = 0;
+    while digit < (32 / exp - 1) && max > base_exp.pow(digit) {
+        let len = output.len();
+        println!("Beginning csort: {}", base_exp.pow(digit));
+        output = counting_sort_bitmask(&mut output, len, base_exp.pow(digit));
+        digit += 1;
+    }
+
+    output
+}
+
+fn  main() {
+    // generate a list of pseudo-random numbers
+    let mut rand: MT = MT::new(6969420);
+
+    let range = 20;
+    let mut arr = vec![0; range];
+    let mut arr2 = vec![0; range];
+    for i in 0..range {
+        arr[i] = rand.next_rand();
+        arr2[i] = rand.next_rand();
+    }
 
     // run radix sort on all of the random numbers
 
-    let arr_o = radix_sort(arr, 10);
+    let arr_o = radix_sort_bitmask(arr, 4);
+    print_vec(&arr_o);
+
+    let arr_o = radix_sort_16(arr2);
     print_vec(&arr_o);
 }
